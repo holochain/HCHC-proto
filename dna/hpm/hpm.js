15a104eac376e406505ab0390d266bfd101ff218
Tisha <tkanjila@gmu.edu>
2017-08-09 17:20:16 -0700

    download app - W.I.P

--- dna/hpm/hpm.js
+++ dna/hpm/hpm.js
@@ -2,68 +2,148 @@ var appDB=[
   {
       "appId":"app1",
       "appName":"Travian",
-      "keywords":["t1","t2"]
-      "desc":"Just another game",
+      "keywords":["t1","t2"],
+      "categories":"category-1",
+      "desc":"Just another game"
   },
   {
       "appId":"app2",
       "appName":"Project IGI",
       "keywords":["IGI1","IGI2"],
-      "desc":"Scary mission",
+      "categories":"category-1",
+      "desc":"Scary mission"
   },
   {
       "appId":"app3",
-      "appName":"Run Meercat Run",
+      "appName":"Temple Run 1",
       "keywords":["MC1","MC2"],
-      "desc":"Join the king without crown",
+      "categories":"category-2",
+      "desc":"Play this game if you like being chased by a wild monkey"
   },
   {
       "appId":"app4",
-      "appName":"quidditch",
-      "keywords":["t1","t2"],
-      "desc":"Play this game if you can fly",
+      "appName":"Temple Run 2",
+      "keywords":["MC1","MC2","MC3"],
+      "categories":"category-2",
+      "desc":"Play this game if you like being chased by a neanderthal"
   },
   {
       "appId":"app5",
+      "appName":"Quidditch",
+      "keywords":["t1","t2","MC1"],
+      "categories":"category-3",
+      "desc":"Play this game if you can fly"
+  },
+  {
+      "appId":"app6",
       "appName":"Age of Empires",
       "keywords":["MC1","MC3"],
-      "desc":"Awesome game based on medieval settings",
+      "categories":"category-3",
+      "desc":"Awesome game based on medieval settings"
   }
 ];
 
 function genesis()
 {
   debug("Starting genesis from hpm")
-  putData(appDB);
+//  call("anchor","addAnchor","");
+  call("anchor","anchor_type_create","category");
+  putData(appDB);// adding from genesis to test display of all apps on pageload()
   return true;
 
 }
 
+/* this function is only testing out the category commit and the app commit */
+/*function putData(appdata)
+{
+  debug("inside putData");
+  var directory = getDirectory();
+  var pivot="skeletonKeyForAllApps";
+  var skeletonKeyHash=commit("key",pivot);
+  var w=commit("skeletonKey_link",{Links:[{Base:directory,Link:skeletonKeyHash,Tag:"key"}]});
+  debug("before for loop starts");
+  for (var i=0;i<appdata.length;i++)
+  {
+    debug("appdata:"+JSON.stringify(appdata[i]));
+    var appHash=commit("app_dna_entry",appdata[i]);
+    //var x=commit("app_links",{Links:[{Base:skeletonKeyHash,Link:appHash,Tag:"app_dna_entry"}]}); // linking all apps to same key
+    var category=appdata[i].categories;
+
+    var checkKey=keywordExists({Anchor_Type:"category",Anchor_Text:category});
+    debug("checkKey"+checkKey);
+    if(checkKey==="")
+    {
+      call("anchor","anchor_create",{Anchor_Type:"category",Anchor_Text:category});
+      debug("anchor for "+category+" created ");
+      var ctgry=makeHash({Anchor_Type:"category",Anchor_Text:category});
+      var categoryLinks=commit("category_links",{Links:[{Base:ctgry ,Link: appHash ,Tag:"app_dna_entry"}]});
+      debug("categoryLinks : "+categoryLinks);
+
+    }else{
+
+      var ctgry=makeHash({Anchor_Type:"category",Anchor_Text:category});
+      var categoryLinks=commit("category_links",{Links:[{Base:ctgry ,Link: appHash ,Tag:"app_dna_entry"}]});
+      debug("linking app to already existing category: "+categoryLinks)
+      debug(" linking  to category :"+appdata[i].categories)
+    }
+
+  //  var categoryLinks=commit("category_links",{Links:[{Base:ctgry ,Link: appHash ,Tag:"app_dna_entry"}]});
+  //  debug("categoryLinks : "+categoryLinks);
+
+
+
+
+
+  }
+
+  fetchByCategory({Anchor_Type:"category",Anchor_Text:"category-2"});
+  //return "success";
+
+}*/
+
+
+
 function putData(appdata)
 {
   debug("inside putData")
-  // traverse through appdata json array
-  // and commit each element using the app id
-  /*
-  for (var i=0;i<appdata.length;i++){
-  debug("Keyword "+(i+1)+" :"+appdata[i].keywords[0]);
-}*/
+
+
+//  for (var i=0;i<appdata.length;i++){
+//  debug("Keyword "+(i+1)+" :"+appdata[i].keywords[0]);
+//}
+  var pivot="skeletonKeyForAllApps";
+  var skeletonKeyHash=commit("key",pivot);
+  var w=commit("skeletonKey_link",{Links:[{Base:directory,Link:skeletonKeyHash,Tag:"key"}]});
 
   for (var i=0;i<appdata.length;i++)
   {
-    //debug("in"+appdata[i])
     var appHash=commit("app_dna_entry",appdata[i]);
     var me=getMe();
-    var x=commit("app_links",{Links:[{Base:me,Link:appHash,Tag:"app"}]});
-    // prints the hash of the commit
-      /*debug("commit "+(i+1)+" with Hash after committing to chain:"+x);
-        debug("Keyword "+(i+1)+" :"+appdata[i].keywords);*/
+    var directory = getDirectory();
+    var x=commit("app_links",{Links:[{Base:skeletonKeyHash,Link:appHash,Tag:"app_dna_entry"}]});
+    var category=appdata[i].categories;
 
-      // link the app to the Keyword
-      //var keyHash=makeHash(appdata[i].keywords);
-    //  var keywordHash=commit("keyword",appdata[i].keywords);
+    /*-------validating category ----------*/
+    var checkCategory=keywordExists({Anchor_Type:"category",Anchor_Text:category});
+    debug("checkKey"+checkKey);
+    if(checkCategory==="")
+    {
+      call("anchor","anchor_create",{Anchor_Type:"category",Anchor_Text:category});
+      debug("anchor for "+category+" created ");
+      var ctgry=makeHash({Anchor_Type:"category",Anchor_Text:category});
+      var categoryLinks=commit("category_links",{Links:[{Base:ctgry ,Link: appHash ,Tag:"app_dna_entry"}]});
+      debug("categoryLinks : "+categoryLinks);
 
-      //check if keyword preexists.if yes, link to existing keyword. if not, then create a new one
+    }else{
+
+      var ctgry=makeHash({Anchor_Type:"category",Anchor_Text:category});
+      var categoryLinks=commit("category_links",{Links:[{Base:ctgry ,Link: appHash ,Tag:"app_dna_entry"}]});
+      debug("linking app to already existing category: "+categoryLinks)
+      debug(" linking  to category :"+appdata[i].categories)
+    }
+
+
+      /*--check if keyword already exists.if yes, link to existing keyword, else create a new one--*/
      for(var k=0;k<appdata[i].keywords.length;k++)
       {
 
@@ -71,14 +151,13 @@ function putData(appdata)
           //debug("checkKey:"+checkKey)
           if(checkKey==="")
           {
-            var directory = getDirectory();
+            //var directory = getDirectory();
             var keywordHash=commit("keyword",appdata[i].keywords[k]);
-            //var keylink=  commit("keyword_links",{Links:[{Base:me,Link:keywordHash,Tag:"keyword"}]});
-            //debug("able to commit keyword links"+keylink)
+
             var drlink= commit("directory_links",{Links:[{Base: directory,Link: keywordHash,Tag:"keyword"}]});
             debug("able to commit directory_links"+drlink)
             var keyapplink=commit("keyword_links",{Links:[{Base: keywordHash,Link: appHash ,Tag:"app_dna_entry"}]});
-            debug("able to commit keyword to individual app"+keyapplink)
+            debug("able to link keyword to individual app:"+keyapplink)
           }else{
             //commit("keyword_links",{Links:[{Base: checkKey,Link: appHash ,Tag:"app_dna_entry"}]});
             var keyHash=makeHash(appdata[i].keywords[k])
@@ -86,32 +165,74 @@ function putData(appdata)
             debug("linking app to already existing keyword: "+x)
             debug("keyword already exists:"+appdata[i].keywords[k])
           }
+
    }
 
 
-      /*
-      var directory = getDirectory();
-
-      var keylink=  commit("keyword_links",{Links:[{Base:me,Link:keywordHash,Tag:"keyword"}]});
-      debug("able to commit keyword links"+keylink)
-      var drlink= commit("directory_links",{Links:[{Base: directory,Link: keywordHash,Tag:"keyword"}]});
-      debug("able to commit directory_links"+drlink)
-      var keyapplink=commit("keyword_links",{Links:[{Base: keywordHash,Link: appHash ,Tag:"app_dna_entry"}]});
-      debug("able to commit keyword to individual app"+keyapplink)
-      */
-
   }
 
- fetchData("t1");
- return "";
+ //fetchData("t1");
+ //fetchAllApps()
+ //fetchByCategory({Anchor_Type:"category",Anchor_Text:"category-3"});
+ //fetchByCategory("category-3");
+ return "success";
 
 }
 
+function fetchByCategory(category)
+{
+    var obj={Anchor_Type:"category",Anchor_Text:category};
+
+    // ----trial code ---//
+    var apps=[];
+    var categoryHash=makeHash(obj);
+    debug("Category"+JSON.stringify(category));
+    //var searchKey=keywordExists(keywordHash);
+   var appList=doGetLinkLoad(categoryHash,"app_dna_entry");
+    //var appLists=getLink(categoryHash,"app_dna_entry",{Load:true});
+    debug("Length of applist"+appList.length)
+
+    for(var j=0;j<appList.length;j++) {
+            var app = appList[j];
+            //debug("applist "+appList[j].H)
+            apps.push(app);
+
+
+      }
+      debug("<--------fetching data by category---------->");
+      debug(JSON.stringify(apps));
+
+      return apps;
+
+}
+
+
+function fetchAllApps()
+{
+  debug("<--------fetching all apps ---------->");
+  var apps=[];
+  //var directory = getDirectory();
+  var skeletonKeyHash=makeHash("skeletonKeyForAllApps");
+  var appList=doGetLinkLoad(skeletonKeyHash,"app_dna_entry");
+  debug("Length of applist"+appList.length)
+  for(var j=0;j<appList.length;j++) {
+          var app = appList[j]
+          //debug("applist "+appList[j].H)
+          apps.push(app);
+
+
+    }
+    debug("All App Data commited to DHT --> "+JSON.stringify(apps));
+    return apps;
+}
+
+
 function fetchData(keyword)
 {
-  debug("<--------fetching data ---------->");
+  debug("<--------fetching data by keyword---------->");
     var apps=[];
     var keywordHash=makeHash(keyword);
+    debug("keyword"+keyword);
     //var searchKey=keywordExists(keywordHash);
    var appList=doGetLinkLoad(keywordHash,"app_dna_entry");
     //var appLists=getLink(keywordHash,"app_dna_entry",{Load:true});
@@ -124,10 +245,17 @@ function fetchData(keyword)
 
       }
       debug("All App DATA --> "+JSON.stringify(apps));
+/*
+      debug("----- Apps by Keyword -----");
+      debug("appHash:"+apps[0].H);
+      debug("app_dna_entry:"+apps[0].app_dna_entry["appName"]);
+      */
+
       //debug("App_dna_entry------->"+apps);
-      debug("type of"+typeof apps);
+      //debug("type of"+typeof apps);
       return apps;
 }
+
 
 
 
@@ -165,6 +293,8 @@ function isErr(result) {
 function doGetLinkLoad(base, tag) {
     // get the tag from the base in the DHT
     var links = getLink(base, tag,{Load:true});
+    debug("Links Raw:"+JSON.stringify(links));
+
     if (isErr(links)) {
         links = [];
     } else {
--- ui/hcscript.js
+++ ui/hcscript.js
@@ -1,54 +1,178 @@
-function searchApp(){
 
-//document.getElementById("searched-val").innerHTML=document.getElementById("searchBar").value;
-var searchParameter=document.getElementById("searchBar").value;
-var array=[
-  {
-      "appId":"app1",
-      "appName":"Travian",
-      "desc":"Just a game",
-  },
-  {
-      "appId":"app2",
-      "appName":"Project IGI",
-      "desc":"Scary mission",
-  },
-  {
-      "appId":"app3",
-      "appName":"King Solomon",
-      "desc":"Join the king community",
-  }
-];
-for(var i=0; i< array.length; i++)
+
+// global variable to contain app related values so it can be debugged in console
+var apps;
+
+var appContainer=document.getElementById("row");
+
+$(document).ready(function(){
+$("#tableMenu a").click(function(e){
+    e.preventDefault(); // cancel the link behaviour
+    $(".row").html("");
+    var selText = $(this).text();
+    $("#tableAnchor").text(selText);
+    listAppByCategory(selText);
+});
+
+});
+
+
+function listAppByCategory(selText)
 {
+  var category=JSON.stringify(selText);
+  alert("Parameter passed:"+selText);
+//  var searchString={Anchor_Type:"category",Anchor_Text:category};
+  send("fetchByCategory",category, function(data) {
+    console.log("printing data"+typeof JSON.parse(data));
 
-    if(searchParameter===array[i].Name)
-    {
-      alert(searchParameter+" found");
-    }
+     apps=JSON.parse(data);
+     alert("apps:"+data);
+     //error handling not working
+     if(! apps[0]){
+       alert("no Results");
+       noResults();
+
+   }else{
+     alert("Results found");
+     for(var i=0;i<apps.length;i++)
+     {
+         var appInfo=JSON.parse(apps[i].app_dna_entry);
+         createHTML(appInfo);
+        //console.log("app Name"+appInfo.appName+":"+appInfo.desc);
+     }
+
+   }
+
+  });
+
+
 }
 
-if(document.getElementById("searchBar").value)
+function searchApp(){
+  var searchInput= JSON.stringify( $('#searchBar').val());
+/* data in function(data) here is the value returned by fetchData */
+  send("fetchData",searchInput, function(data) {
+    console.log("printing data"+typeof JSON.parse(data));
+
+     apps=JSON.parse(data);
+    // alert("apps:"+data);
+     //error handling not working
+     if(! apps[0]){
+       //alert("no Results");
+       $(".row").html("");
+       noResults();
+
+   }else{
+    // alert("Results found");
+     $(".row").html("");
+     for(var i=0;i<apps.length;i++)
+     {
+         var appInfo=JSON.parse(apps[i].app_dna_entry);
+         createHTML(appInfo);
+        //console.log("app Name"+appInfo.appName+":"+appInfo.desc);
+     }
+
+   }
+
+  });
+
+
+
+}
+
+
+
+function findAllApps(){
+  var searchInput= JSON.stringify( $('#searchBar').val());
+// data in function(data) here is the value returned by fetchData
+  send("fetchAllApps",searchInput, function(data) {
+    console.log("printing data"+typeof JSON.parse(data));
+
+     apps=JSON.parse(data);
+     //alert("apps:"+data);
+     //error handling not working
+     if(! apps[0]){
+       //alert("no Results");
+       noResults();
+
+   }else{
+     //alert("Results found");
+     for(var i=0;i<apps.length;i++)
+     {
+         var appInfo=JSON.parse(apps[i].app_dna_entry);
+         //createHTML(appInfo);   <-- uncomment this and comment addApp(appInfo) if this doesnt work
+         addApp(appInfo);
+        //console.log("app Name"+appInfo.appName+":"+appInfo.desc);
+     }
+
+   }
+
+  });
+
+
+
+}
+/*testing out return of dom elements  from function*/
+function addApp(appInfo)
 {
+//  var addApp=document.querySelector("div.row");
+  $("div.row").prepend(makeAppHTML(appInfo));
+}
+function makeAppHTML(appInfo)
+{
+  var appID,appName,appDesc,appCategory;
+
+ appName=appInfo.appName;
+ appID=appName+1;
+ appDesc=appInfo.desc;
+ appCategory=appInfo.category
+
+ return '<div class="col-md-3" id="'+appID+'"><div class="media"><a href="#" class="thumbnail pull-left"><img src="images/smartBear.jpg" alt="App Logo" width="250px" height="150px"/></a><div class="media-body"><h5>'+appName+'</h5></div></div></div>'
+
+}
+/* end of test*/
+function createHTML(appInfo){
+//alert("createHTML");
   var appElement= document.createElement('div');
-  appElement.class="col-sm-4";
-  appElement.style.width="400px";
-  appElement.style.height="110px";
-  appElement.style.margin="5px 5px 10px 135px";
-  appElement.style.background="powderblue";
-  var appH1= document.createElement('h3');
-  appH1.innerText="App 1";
+  appElement.class="col-md-4";
+  //appElement.style.margin="5px 5px 10px 135px";
+  var appH1= document.createElement('h4');
+  appH1.innerText=appInfo.appName;
 
   var appP1= document.createElement('p');
   appP1.innerText="Add an Image";
   var appP2= document.createElement('p');
-  appP2.innerText="And brief description..";
+  appP2.innerText=appInfo.desc;
+  var addApp=document.querySelector("div.row");
+  //addApp.innerText="no results found";
 
-  document.body.appendChild(appElement);
+  var bttn=document.createElement("BUTTON");
+  bttn.class="btn btn-info";
+  var node=document.createTextNode("download");
+  bttn.appendChild(node);
+  //bttn.class="btn btn-success";
+
   appElement.appendChild(appH1);
   appElement.appendChild(appP1);
   appElement.appendChild(appP2);
+  appElement.appendChild(bttn);
+  addApp.prepend(appElement);
+
 
 }
 
+function noResults()
+{
+  var appElement= document.createElement('div');
+  appElement.class="alert alert-warning";
+  appElement.role="alert";
+  appElement.style.margin="5px 5px 10px 135px";
+  var appH1= document.createElement('h3');
+  appH1.innerText="No results Found..";
+  var appP1= document.createElement('p');
+  appP1.innerText="Add an Image";
+  var addApp=document.querySelector("div.row");
+  appElement.appendChild(appH1);
+  appElement.appendChild(appP1);
+  addApp.prepend(appElement);
 }
